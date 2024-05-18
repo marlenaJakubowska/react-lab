395bd21f61b77b7fde07e3c54edfd7274c0629a6
marlenaJakubowska <mar.jakubowska90@gmail.com>
2024-05-18 13:32:56 +0200

    set email and print message based on email length

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'adres@emailpl';
+    const [email, setEmail] = useState('adres@email.pl');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    let message;
+    if (email.length < 10) {
+        message = <div>Hello shorty!</div>;
+    } else if (email.length < 15) {
+        message = <div>You're a charm</div>;
+    } else {
+        message = <div>I feel asleep reading that</div>;
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            {message}
+            <input type="text" value={email} onChange={handleChange}/>
         </div>
     );
 }
